122efa3c198086e245f294db9fa12a4adefb623b
omjpro <omjcrow@naver.com>
2021-12-31 22:43:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.recyclerview.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        //리스트 카운트
+//        val textView : TextView = findViewById(R.id.textview)
+//        textView.text = Datasource().loadAffirmation().size.toString()
+        
+        
+        
+        
+        
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerview.adapter
+
+import android.content.Context
+import com.example.recyclerview.model.Affirmation
+
+class ItemAdapter(private val context : Context, private val dataset : List<Affirmation>) {
+    
+    class ItemViewHolder()
+}
