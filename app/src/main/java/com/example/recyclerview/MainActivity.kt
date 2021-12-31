beed94ff9585d1cc49d44d9498abbad6adf64857
omjpro <omjcrow@naver.com>
2021-12-31 23:57:25 +0900

    draft

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.adapter.ItemAdapter
 import com.example.recyclerview.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val myDataset = Datasource().loadAffirmations()
+        
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true) //콘텐츠 내용변경에도 recyclerView 레이아웃 크기 변동X 경우, 성능개선
         
         //리스트 카운트
 //        val textView : TextView = findViewById(R.id.textview)
--- app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
@@ -1,9 +1,39 @@
 package com.example.recyclerview.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.R
 import com.example.recyclerview.model.Affirmation
 
-class ItemAdapter(private val context : Context, private val dataset : List<Affirmation>) {
+class ItemAdapter(
+    private val context : Context, 
+    private val dataset : List<Affirmation>
+    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     
-    class ItemViewHolder()
+    class ItemViewHolder(private val view : View) : RecyclerView.ViewHolder(view) {
+        val textView : TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout) //루트뷰가 adapterLayout인 새 ItemViewHolder인스턴스를 반환(생성)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        //뷰홀더내의 텍스트뷰에 item에 들어있는 문자열리소스ID로 getString()을 통해 인스턴스 참조해서 셋함
+        holder.imageView.setImageResource(item.imageResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    } //간소화 override fun getItemCount() = dataset.size 
 }
